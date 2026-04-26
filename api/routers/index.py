821805748a78ec5dc87ed11167befad0eb0e1754
kyle <krahilly@charlotte.edu>
2026-04-26 18:19:50 -0400

    adjusted router order

--- api/routers/index.py
+++ api/routers/index.py
@@ -4,9 +4,9 @@ from . import orders, order_details, menu, promotions, customers, payments, reso
 def load_routes(app):
     app.include_router(customers.router)
     app.include_router(menu.router)
-    app.include_router(promotions.router)
     app.include_router(orders.router)
     app.include_router(order_details.router)
+    app.include_router(promotions.router)
     app.include_router(payments.router)
     app.include_router(resources.router)
 
