2d0a336d6bb0dab501c4532e0e65f2ec73036222
Lalith Gundapaneni <your_email@charlotte.edu>
2026-04-26 18:35:18 -0400

    Add missing router registrations to index

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,4 +1,5 @@
-from . import orders, order_details, menu, promotions
+from . import (orders, order_details, menu, promotions,
+               analytics, customers, payments, ratings, resources)
 
 
 def load_routes(app):
@@ -6,4 +7,8 @@ def load_routes(app):
     app.include_router(promotions.router)
     app.include_router(orders.router)
     app.include_router(order_details.router)
-
+    app.include_router(analytics.router)
+    app.include_router(customers.router)
+    app.include_router(payments.router)
+    app.include_router(ratings.router)
+    app.include_router(resources.router)
